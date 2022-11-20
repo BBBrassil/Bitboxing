495637f96216e83d301062fec5f478f38ee76936
mikkelsonmatwit <71507351+mikkelsonmatwit@users.noreply.github.com>
2022-11-20 01:00:44 -0500

    Contains everything except leaderboard

--- database version/bitboxing_client.py
+++ database version/bitboxing_client.py
@@ -8,76 +8,230 @@ from tkinter import *
 from functools import partial
 import bitboxing_message_formatting
 import socket
-import login_window
+#import login_window
 
 HOST = '127.0.0.1' #socket.gethostname()
 
 PORT = 9999
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-username = 'not logged in'
+NO_USER = 'not logged in'
 
 root = Tk()
 root.title('Main')
 root.geometry('500x300')
 
 
-
-
-
-def openNewUser():
-    newUserWindow.mainloop
-    loginWindow.destroy
-
-
-def openLoginWin():
-    loginWindow = Toplevel(root)
-    loginWindow.geometry('200x200')
-    loginWindow.title('Login')
-        
-    Label(loginWindow, text='Login to Bitboxing').pack()
-        
-    Label(loginWindow, text='Username').pack()
-    loginUsername = Entry(loginWindow)
-    loginUsername.pack()
-        
-    Label(loginWindow, text='Password').pack()
-    loginPassword = Entry(loginWindow)
-    loginPassword.pack()
-        
-    loginErrorLabel = Label(loginWindow, text='')
-        
-    loginBtn = Button(loginWindow, text='login', command=partial(login, loginWindow, loginUsername, loginPassword, loginErrorLabel))
-    loginBtn.place(x=25, y=135)
-        
-    newUsrBtn = Button(loginWindow, text='new user', command=openNewUser)
-    newUsrBtn.place(x=75, y=135)
-        
-    loginQuitBtn = Button(loginWindow, text='go back', command=loginWindow.destroy)
-    loginQuitBtn.place(x=145, y=135)
-        
-    loginErrorLabel.pack()
     
-def login(window, usernamel, passwordl, label):
-    u = usernamel.get()
-    p = passwordl.get()
+def login():
+    u = usernamelbl.get()
+    p = passwordlbl.get()
     bitboxing_message_formatting.send_msg(s, bitboxing_message_formatting.format_request(u, "LOGIN", p))
     response = bitboxing_message_formatting.parse_response(bitboxing_message_formatting.receive_msg(s))
     if response[0] == "0":
-        username = u
-        window.destroy()
+        usernameRoot["text"] = "user: " + u
+        errorLbl["text"] = ''
     else:
-        label["text"] = response[1]
+        errorLbl["text"] = response[1]
 
-def refresh():
-    print(username)
-    usernameRoot["text"] = username
+def newUser():
+    u = usernamelbl.get()
+    p = passwordlbl.get()
+    bitboxing_message_formatting.send_msg(s, bitboxing_message_formatting.format_request(u, "NEW_USER", p))
+    response = bitboxing_message_formatting.parse_response(bitboxing_message_formatting.receive_msg(s))
+    if response[0] == "0":
+        errorLbl["text"] = "user successfully created"
+        usernameRoot["text"] = "user: " + u
+    else:
+        errorLbl["text"] = response[1]
 
-Label(root, text='Choose an option').place(x=0,y=0)
-usernameRoot = Label(root, text=username).place(x=0,y=25)
-openLoginBtn = Button(root, text='login / switch user', command=openLoginWin).place(x=30, y=50)
-refreshBtn = Button(root, text='refresh', command=refresh).place(x=60, y=80)
-quitBtn = Button(root, text='Quit', command=root.destroy).place(x=60, y=110)
+def selectProtocol():
+    
+    if len(protocols.curselection()) < 1:
+        errorLbl["text"] = "no protocol selected"
+    elif len(protocols.curselection()) > 1:
+        errorLbl["text"] = "too many protocols selected"
+    else:
+        #get which protocol is being choosen
+        command = protocols.curselection()[0]
+        #check if leaderboard
+        if(command == 3):
+            #run leaderboard
+            print("leaderboard")
+        else:
+            #if not, verify logged in
+            if(usernameRoot["text"] == NO_USER):
+                print("not logged in")
+                errorLbl["text"] = "Must login first"
+            else:
+                #open appropriate window
+                if(command == 0):
+                    findWindow(usernameRoot["text"])
+                elif(command == 1):
+                    #run solve
+                    solveWindow(usernameRoot["text"])
+                elif(command == 2):
+                    #run hint
+                    hintWindow(usernameRoot["text"])
+                elif(command == 4):
+                    #run stats
+                    statsWindow(usernameRoot["text"])
+    
+'''
+protocol Windows
+'''
+
+'''
+FIND PROTOCOL
+'''
+def findPuzzle(label, entry, u):
+    p = entry.get()
+    bitboxing_message_formatting.send_msg(s, bitboxing_message_formatting.format_request(u, "FIND", p))
+    response = bitboxing_message_formatting.parse_response(bitboxing_message_formatting.receive_msg(s))
+    label["text"] = response[1]
+
+def findWindow(username):
+
+    findWindow = Toplevel(root)
+    findWindow.title("Found Puzzle")
+    findWindow.geometry("350x250")
+
+    Label(findWindow, text="Enter puzzle number").pack()
+
+    puzzleEntry = Entry(findWindow)
+    puzzleEntry.pack()
+    
+    output = Label(findWindow, text="placing")
+    output.place(x=10, y=100)
+
+    submitBtn = Button(findWindow, text="submit", command=partial(findPuzzle, output, puzzleEntry, username))
+    submitBtn.pack()
+    
+    backBtn = Button(findWindow, text="Go Back", command=findWindow.destroy)
+    backBtn.pack()
+
+    
+    
+    findWindow.mainloop()
+
+'''
+HINT PROTCOL
+'''   
+
+def getHint(label, entry, u):
+    p = entry.get()
+    bitboxing_message_formatting.send_msg(s, bitboxing_message_formatting.format_request(u, "HINT", p))
+    response = bitboxing_message_formatting.parse_response(bitboxing_message_formatting.receive_msg(s))
+    label["text"] = response[1]
+
+def hintWindow(username):
+
+    hintWindow = Toplevel(root)
+    hintWindow.title("Request Hint")
+    hintWindow.geometry("350x250")
+
+    Label(hintWindow, text="Enter puzzle number").pack()
+
+    puzzleEntry = Entry(hintWindow)
+    puzzleEntry.pack()
+    
+    output = Label(hintWindow, text="placing")
+    output.place(x=10, y=100)
+
+    submitBtn = Button(hintWindow, text="submit", command=partial(getHint, output, puzzleEntry, username))
+    submitBtn.pack()
+    
+    backBtn = Button(hintWindow, text="Go Back", command=hintWindow.destroy)
+    backBtn.pack()
+
+    hintWindow.mainloop()
+    
+'''   
+SOLVE PROTOCOL
+'''
+
+def solvePuzzle(label, entryP, entryS, u):
+    puzzle = entryP.get()
+    solution = entryS.get()
+    bitboxing_message_formatting.send_msg(s, bitboxing_message_formatting.format_request(u, "SOLVE", puzzle, solution))
+    response = bitboxing_message_formatting.parse_response(bitboxing_message_formatting.receive_msg(s))
+    label["text"] = response[1]
+
+def solveWindow(username):
+    solveWindow = Toplevel(root)
+    solveWindow.title("Request Hint")
+    solveWindow.geometry("350x250")
+
+    Label(solveWindow, text="Enter puzzle number").pack()
+
+    puzzleEntry = Entry(solveWindow)
+    puzzleEntry.pack()
+    
+    Label(solveWindow, text="Enter solution").pack()
+    
+    solutionEntry = Entry(solveWindow)
+    solutionEntry.pack()
+    
+    output = Label(solveWindow, text="placing")
+    output.place(x=10, y=150)
+
+    submitBtn = Button(solveWindow, text="submit", command=partial(solvePuzzle, output, puzzleEntry, solutionEntry, username))
+    submitBtn.pack()
+    
+    backBtn = Button(solveWindow, text="Go Back", command=solveWindow.destroy)
+    backBtn.pack()
+
+    solveWindow.mainloop()
+
+'''
+STATS PROTOCOL
+'''
+
+def statsWindow(username):
+    statsWindow = Toplevel(root)
+    statsWindow.title("User Stats")
+    statsWindow.geometry("350x350")
+    
+    bitboxing_message_formatting.send_msg(s, bitboxing_message_formatting.format_request(username, "STATS"))
+    response = bitboxing_message_formatting.parse_response(bitboxing_message_formatting.receive_msg(s))
+    
+    Label(statsWindow, text=response[1], justify=LEFT).pack()
+    
+    backBtn = Button(statsWindow, text="Go Back", command=statsWindow.destroy)
+    backBtn.pack(side=BOTTOM)
+    
+    statsWindow.mainloop()
+
+'''
+    
+def leaderboardWindow():
+    
+'''
+Label(root, text='Choose an option').pack()
+usernameRoot = Label(root, text=NO_USER)
+usernameRoot.place(x=10,y=25)
+
+Label(root, text='Username').place(x=10, y=50)
+usernamelbl = Entry(root)
+usernamelbl.place(x=10, y=75)
+    
+Label(root, text='Password').place(x=10, y=100)
+passwordlbl = Entry(root)
+passwordlbl.place(x=10, y=125)
+    
+errorLbl = Label(root, text='')
+errorLbl.place(x=10, y=150)
+    
+loginBtn = Button(root, text='login', command=login)
+loginBtn.place(x=10, y=175)
+    
+newUsrBtn = Button(root, text='create new user', command=newUser)
+newUsrBtn.place(x=60, y=175)
+
+
+
+
+quitBtn = Button(root, text='Quit', command=root.destroy).place(x=10, y=205)
 
 protocols = Listbox(root)
 protocols.insert(1, 'Found Item')
@@ -85,7 +239,10 @@ protocols.insert(2, 'Solve Puzzle')
 protocols.insert(3, 'Request Hint')
 protocols.insert(4, 'Open Leaderboard')
 protocols.insert(5, 'My stats')
-protocols.insert(6, 'logout')
+
+selectBtn = Button(root, text='select protocol', command=selectProtocol)
+selectBtn.pack()
+
 protocols.pack()
 
 root.mainloop()
